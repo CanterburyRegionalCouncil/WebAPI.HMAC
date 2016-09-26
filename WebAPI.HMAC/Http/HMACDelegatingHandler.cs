c1ce06dd60156ef612e301749d542d8fdd8d4433
Gavin Harriss <mail@gavinharriss.com>
2016-09-26 16:09:56 +1300

    Bug fix for large post payloads described in http://stackoverflow.com/questions/38966981

--- WebAPI.HMAC/Http/HMACDelegatingHandler.cs
+++ WebAPI.HMAC/Http/HMACDelegatingHandler.cs
@@ -20,7 +20,7 @@ namespace WebAPI.HMAC.Http
             InnerHandler = new HttpClientHandler();
         }
 
-        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             // Calculate UNIX time.
             var epochStart = new DateTime(1970, 01, 01, 0, 0, 0, 0, DateTimeKind.Utc);
@@ -31,7 +31,7 @@ namespace WebAPI.HMAC.Http
             var nonce = HMACHelper.BuildNonce();
 
             // Build the base 64 signature.
-            var base64Signature = await HMACHelper.BuildBase64Signature(
+            var base64Signature = HMACHelper.BuildBase64Signature(
                 _apiKey,
                 _appId,
                 request.RequestUri,
@@ -39,13 +39,13 @@ namespace WebAPI.HMAC.Http
                 request.Content,
                 nonce,
                 requestTimeStamp
-                );
+            ).Result;
 
             //Setting the values in the Authorization header using custom scheme (amx)
             request.Headers.Authorization = new AuthenticationHeaderValue("amx",
                 $"{_appId}:{base64Signature}:{nonce}:{requestTimeStamp}");
 
-            var response = await base.SendAsync(request, cancellationToken);
+            var response = base.SendAsync(request, cancellationToken);
 
             return response;
         }
